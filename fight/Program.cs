1a4572e06e99f4c3a44af35c2e0871c277f145e4
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2024-09-24 12:48:34 +0200

    first commit

---
+++ fight/Program.cs
@@ -0,0 +1,34 @@
+﻿Random rnd = new Random();
+int dålig = 50;
+int bra = 50;
+int punch = 0;
+int punch2 = 0;
+int round = 0;
+Console.WriteLine("start fight");
+Console.ReadLine();
+while (dålig > 0 & bra > 0)
+{
+    round++;
+    Console.WriteLine($"round{round}");
+    Console.WriteLine();
+    punch = rnd.Next(1, 10);
+    punch2 = rnd.Next(1, 10);
+    Console.WriteLine("you punch child");
+    dålig -= punch;
+    Console.WriteLine($"child hp = {dålig}");
+    Console.WriteLine();
+    Console.WriteLine("child punch you");
+    bra -= punch2;
+    Console.WriteLine($"your hp = {bra}");
+    Console.ReadLine();
+}
+if(dålig > bra){
+Console.WriteLine("you lose");
+}
+if(dålig == bra){
+ Console.WriteLine("tie");   
+}
+if(dålig < bra){
+Console.WriteLine("you win");
+}
+Console.ReadLine();
